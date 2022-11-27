caa8c14dc85c71ae9523d42d6e810c762968a847
Jaydenchao71 <3461926217@qq.com>
2022-11-27 21:13:25 +0800

    easypython

--- example.py
+++ example.py
@@ -1,4 +1,3 @@
 from script import *
-dir()
 print(raninteger(8, 10))
 print(ran0t1)
--- script.py
+++ script.py
@@ -1,8 +1,16 @@
 import os
 import datetime
 import random
+from tkinter import Tk
+from tkinter import *
+import turtle
+import webbrowser
+import logging
+import math
+f = False
+t = True
 echo = print
-openapp = os.system
+command = os.system
 delf = os.remove
 dir = os.listdir
 md = os.mkdir
@@ -15,3 +23,20 @@ isoformat2 = today.isoformat() #print(isoformat)
 ctime2 = today.ctime() #print(ctime)
 raninteger = random.randint #print(raninteger)
 ran0t1 = random.random() #print(ran0t1)
+window = Tk()
+tktico = window.iconbitmap
+tkt = window.title
+tksize = window.geometry
+tkgui = window.mainloop
+tkbs = window.resizable
+screen = turtle.Screen
+#url = "http://google.com"
+openurl = webbrowser.open #openurl(url)
+opennurl = webbrowser.open_new #opennurl(url)
+opennewtab = webbrowser.open_new_tab #opennewtab(url)
+pi = math.pi
+debugmsg = logging.debug
+infomsg = logging.info
+warnmsg = logging.warn
+errmsg = logging.error
+criticalmsg = logging.critical
