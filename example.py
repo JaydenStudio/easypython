2a0edd87a2a39fcf7f24cde1bcaaf70a263bd132
Jaydenchao71 <3461926217@qq.com>
2022-11-27 18:35:50 +0800

    easypython

---
+++ example.py
@@ -0,0 +1,4 @@
+from script import *
+dir()
+print(raninteger(8, 10))
+print(ran0t1)
---
+++ script.py
@@ -0,0 +1,17 @@
+import os
+import datetime
+import random
+echo = print
+openapp = os.system
+delf = os.remove
+dir = os.listdir
+md = os.mkdir
+today = datetime.date.today() #print(today)
+year = today.year #print(year)
+mouth = today.month #print(mouth)
+weekday2 = today.weekday() #print(weekday)
+isoweekday2 = today.isoweekday() #print(isoweekday)
+isoformat2 = today.isoformat() #print(isoformat)
+ctime2 = today.ctime() #print(ctime)
+raninteger = random.randint #print(raninteger)
+ran0t1 = random.random() #print(ran0t1)
